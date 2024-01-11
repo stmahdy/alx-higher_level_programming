a1ecccebffa53e717e947dbeeb56a88ba184b7d6
stmahdy <statimahdi@outlook.com>
2024-01-11 14:15:34 -0800

    my commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in my_list:
-        if i == search:
-            ind = my_list.index(search)
-            my_list[ind] = replace
-    return my_list
+    return list(map(lambda e: replace if e == search else e, my_list))
