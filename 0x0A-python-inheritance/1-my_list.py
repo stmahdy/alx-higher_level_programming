3103248236bc683ea1a40c7f2116427e5bd5dc5b
stmahdy <statimahdi@outlook.com>
2024-02-05 13:58:39 -0800

    my commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-
 """
-====================================================
-=                   My class                       =
-==================================================== 
+===========================
+Module with class MyList
+===========================
 """
 
 
-class MyList:
-        """Class with method print_sorted"""
-        pass
+class MyList(list):
+    """Class with method print_sorted"""
+    pass
 
     def print_sorted(self):
         """Methot that sorted a list"""
