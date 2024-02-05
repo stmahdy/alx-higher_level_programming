9b75dedaf3ac11d57027f304a24ad9d245871cf6
stmahdy <statimahdi@outlook.com>
2024-02-05 13:47:47 -0800

    my commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+====================================================
+=                   My class                       =
+==================================================== 
+"""
+
+
+class MyList:
+        """Class with method print_sorted"""
+        pass
+
+    def print_sorted(self):
+        """Methot that sorted a list"""
+
+        print(sorted(list(self)))
