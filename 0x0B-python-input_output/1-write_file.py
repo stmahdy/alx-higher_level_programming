fdc95c15aa4526cc0f918f685ab6a57fd5097823
stmahdy <statimahdi@outlook.com>
2024-02-06 13:48:09 -0800

    my commit

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define function to write into a file"""
+
+
+def number_of_lines(filename=""):
+    """ function that returns the number of lines of a text file """
+
+    with open(filename) as f:
+        count = len(f.readlines())
+    return count
