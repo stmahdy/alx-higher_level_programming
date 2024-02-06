590110944b49598b8d1e6d987822676bebddc37e
stmahdy <statimahdi@outlook.com>
2024-02-06 13:58:39 -0800

    eliminating space

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """ function that returns the number of lines of a text file """
-
+    """ function that returns the number of lines of a text file """I
     with open(filename, "w", encoding='utf-8') as f:
         return f.write(text)
