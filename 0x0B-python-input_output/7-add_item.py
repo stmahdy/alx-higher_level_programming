511259a731a2075fb1bc220561f86781a9f35e8e
stmahdy <statimahdi@outlook.com>
2024-02-07 12:24:41 -0800

    My commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ import os.path
 import sys
 from sys import argv
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
 filename = "add_item.json"
 json_list = []
