28ac63d80c18b690ef81b2ca654ee1078dbed1e4
stmahdy <statimahdi@outlook.com>
2024-02-07 12:53:31 -0800

    my commit

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,8 +2,8 @@
  """ function that returns the number of lines of a text file """
 
 
+
 def write_file(filename="", text=""):
-    """ function that returns the number of lines of a text file """
-    
+    """ function that returns the number of lines of a text file """ 
     with open(filename, "w", encoding='utf-8') as f:
         return f.write(text)
